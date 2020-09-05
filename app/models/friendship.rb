64e5ca092861198a918d0af2aaf401375c179ed1
Nick <nsardonini@gmail.com>
2020-09-05 19:54:02 -0400

    add method to send friend request
    method creates two Friendship objects, one for the sender that has an 'accepted' value of true,
    as the sender of the request implies their intention of becoming friends via the request. the other Friendship object has the default 'accepted' value, as it depends on the response
    of the receiver of the request.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,4 +4,13 @@ class Friendship < ApplicationRecord
 
   scope :accepted_friendships, -> { where(accepted: true) }
   scope :pending_friendships, -> { where(accepted: false) }
+
+  private
+
+  def self.send_friend_request user_id, friend_id
+    # User who requests friendship defaults to accepting the Friend Request
+    Friendship.create user_id: user_id, friend_id: friend_id, accepted: true
+    # Reciprocate request from other 'side' of friendship
+    Friendship.create user_id: friend_id, friend_id: user_id
+  end
 end
