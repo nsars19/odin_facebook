eca0f9c21516009665e9ee349ae502bc5bebdce0
Nick <nsardonini@gmail.com>
2020-09-06 16:32:39 -0400

    add #create_notification method for after_create callback

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,4 @@
 class Friendship < ApplicationRecord
-  after_create :create_notification
-  
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
@@ -11,6 +9,14 @@ class Friendship < ApplicationRecord
 
   def self.send_friend_request user_id, friend_id
     # User who requests friendship defaults to accepting the Friend Request
+  def create_notification
+    Notification.create(
+      receiver_id: self.friend.id,
+      sender_id: self.user.id,
+      notifiable: self
+      ) if self.accepted
+  end
+
     Friendship.create user_id: user_id, friend_id: friend_id, accepted: true
     # Reciprocate request from other 'side' of friendship
     Friendship.create user_id: friend_id, friend_id: user_id
