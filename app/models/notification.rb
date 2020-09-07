4286d6397ca78c7f698b62085629e44a67ece268
Nick <nsardonini@gmail.com>
2020-09-07 18:53:49 -0400

    add after_find callback to Notification
    When a user's notifications are pulled from the database (ie. when they are accessed via the '/notifications' path)
    their read value is set to true, and their read_at value is changed from 'nil' to the time of viewing.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,17 @@
 class Notification < ApplicationRecord
+  after_find :mark_as_read
+  
   belongs_to :receiver, class_name: "User"
   belongs_to :sender, class_name: "User"
   belongs_to :notifiable, polymorphic: true
 
   scope :unread, -> { where(read_at: nil) }
+
+  private
+
+  def mark_as_read
+    self.read = true
+    self.read_at = DateTime.now
+    self.save
+  end
 end
