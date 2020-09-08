890d4361700f20bcb33b1828c9619e0dd01cc294
Nick <nsardonini@gmail.com>
2020-09-07 21:41:34 -0400

    add #create & strong params method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    
+    if @post.save
+      redirect_to home_path
+    else
+      render home_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
