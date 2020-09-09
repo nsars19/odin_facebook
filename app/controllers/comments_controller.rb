ee9f00fe49eef00c6f657ff287447ba09588a8f9
Nick <nsardonini@gmail.com>
2020-09-09 18:54:25 -0400

    add #create for Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    comment = params[:comment][:comment]
+    @comment = Comment.new(
+      post_id: params[:post_id],
+      user_id: current_user.id,
+      text: comment
+    )
+
+    if @comment.save
+      redirect_back(fallback_location: 'posts/index')
+    end
+  end
 end
