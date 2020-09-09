0243ed92441afe2297732ab0d54407d149a754f0
Nick <nsardonini@gmail.com>
2020-09-08 21:20:31 -0400

    add after_create callback to notify Poster that their Post was Liked
    creates a notification for the post creator when their post is liked. if the user likes their own post
    then no notification is sent to that user.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,21 @@
 class Like < ApplicationRecord
+  after_create :create_notification, unless: :liked_own_post?
+  
   belongs_to :post
   belongs_to :user
+
+  def create_notification
+    post_creator_id = Post.find(self.post_id).user_id
+
+    Notification.create(
+      receiver_id: post_creator_id,
+      sender_id: self.user.id,
+      notifiable: self
+    )
+  end
+
+  def liked_own_post?
+    post_author_id = Post.find(self.post_id).user_id
+    post_author_id == self.user_id
+  end
 end
