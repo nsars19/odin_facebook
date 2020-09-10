70067ab62ec168b23732350027893185ed7ec70c
Nick <nsardonini@gmail.com>
2020-09-10 14:37:27 -0400

    add helper method for #create
    adds #already_requested? which checks if a Friendship request has already been sent from one user to another

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,4 +9,13 @@ class FriendshipsController < ApplicationController
       format.js { render inline: "location.reload();" }
     end
   end
+
+  private
+
+  def already_requested?
+    receiver_id = params[:user_id]
+    sender_id = current_user.id
+
+    Friendship.where(friend_id: receiver_id, user_id: sender_id).any?
+  end
 end
