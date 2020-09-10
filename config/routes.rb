d8dda7ddfd2e5901b4f76b96a23a08632ce39004
Nick <nsardonini@gmail.com>
2020-09-10 14:42:51 -0400

    add friendships resources nested under users resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index', as: 'home'
 
-  resources :users, only: [:index, :show]
   resources :notifications, only: :index
+  
+  resources :users, only: [:index, :show] do
+    resources :friendships
+  end
+  
   resources :posts, only: [:create, :index] do
     resources :comments
     resource :like, only: [:new, :create, :destroy]
