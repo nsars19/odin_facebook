0cce853f40cd870c6680dd6ba1c65814dcccb1f2
Nick <nsardonini@gmail.com>
2020-09-10 14:37:04 -0400

    add #create in friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,12 @@
 class FriendshipsController < ApplicationController
+  def create
+    receiver_id = params[:user_id]
+    sender_id = current_user.id
+
+    Friendship.send_friend_request(sender_id, receiver_id) unless already_requested?
+
+    respond_to do |format|
+      format.js { render inline: "location.reload();" }
+    end
+  end
 end
