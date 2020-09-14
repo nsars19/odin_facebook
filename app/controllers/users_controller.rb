ef2237e1c07f04f747ec378cbd22cdcee0d223fe
Nick <nsardonini@gmail.com>
2020-09-14 19:36:00 -0400

    update users variable in userscontroller#index
    changed users from all users except current user, to all users minus current user, current user's friends,
    and users who have a pending friend request from the current user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.reject { |user| user == current_user }
+    @users = User.all.reject { |user| user == current_user } - 
+      current_user.friends -
+      current_user.pending_friendships.map do |request|
+          User.find request.friend_id
+        end
   end
 
   def show
