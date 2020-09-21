b558c7d759911986add8c99e2f1cc4d9769e49dc
Nick <nsardonini@gmail.com>
2020-09-21 19:07:28 -0400

    continue configuring oauth-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,22 @@ class User < ApplicationRecord
     likes = Like.where(user_id: self.id, post_id: post.id)
     likes.any?
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      # user.image = auth.info.image
+      # uer.skip_confirmation!
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'posts#index', as: 'home'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :notifications, only: :index
   
