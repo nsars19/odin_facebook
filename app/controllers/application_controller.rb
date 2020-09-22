2c44868bba70c826e65f46f53ba3947416d79dbf
Nick <nsardonini@gmail.com>
2020-09-22 15:22:10 -0400

    add strong params for password confirmation & avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
-end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password, :password_confirmation, :avatar)
+    end
+  end
 end
