026d9c8f51affb8ad3c53a8e7b4cd65e2c4db571
Nick <nsardonini@gmail.com>
2020-09-24 19:19:11 -0400

    configured User's Devise registration controller for the User Mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'posts#index', as: 'home'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
 
   resources :notifications, only: :index
   
