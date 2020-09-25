bd5e2ff816b723e764227459807aeaaf828f2ad4
Nick <nsardonini@gmail.com>
2020-09-25 14:14:23 -0400

    configure SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure ActionMailer to point to SendGrid's servers
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'https://young-earth-27505.herokuapp.com/',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Configure ActionMailer/SendGrid
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://young-earth-27505.herokuapp.com/" }
 end
