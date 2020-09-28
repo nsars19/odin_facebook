e10ee2d7cea331725c0f697b15bdc599a85caaf6
Nick <nsardonini@gmail.com>
2020-09-28 14:57:27 -0400

    refactor User welcome email to use SendGrid gem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,14 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    mail(to: @user.email, subject: "Thanks for joining Odin Facebook!")
+    subject = 'Thanks for joining Odin Facebook!'
+    from = Email.new(email: 'welcome@odin_facebook.com')
+    to = Email.new(email: @user.email)
+    email_content = "Thanks for joining Odin Facebook, #{@user.name}! We hope you have a great time!"
+    content = Content.new(type: 'text/plain', value: email_content)
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').pots(request_body: mail.to_json)
   end
 end
