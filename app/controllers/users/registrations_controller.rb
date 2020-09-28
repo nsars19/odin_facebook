8aab3bb9d8c63cadb4344749b0fd22d1a8899330
Nick <nsardonini@gmail.com>
2020-09-28 14:54:19 -0400

    refactor welcome email to use SendGrid gem

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,7 +12,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     super do |user|
-      UserMailer.with(user: user).welcome_email.deliver_later
+      subject = 'Thanks for joining Odin Facebook!'
+      from = Email.new(email: 'welcome@odin_facebook.com')
+      to = Email.new(email: user.email)
+      email_content = "Thanks for joining Odin Facebook, #{user.name}! We hope you have a great time!"
+      content = Content.new(type: 'text/plain', value: email_content)
+      mail = SendGrid::Mail.new(from, subject, to, content)
+
+      sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+      response = sg.client.mail._('send').pots(request_body: mail.to_json)
     end
   end
 
